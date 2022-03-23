4a1020546b88e491e7c3b337ccc3a4fbe8ced091
raja <raja.rakoto7@gmail.com>
2022-03-23 18:13:16 +0300

    reorganize vantaJS

---
+++ vantaJS/vanta.js
@@ -0,0 +1,10 @@
+VANTA.WAVES({
+  el: "#vanta-waves",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 600.00, 
+  minWidth: 100.00, 
+  scale: 1.00,
+  scaleMobile: 1.00
+})
