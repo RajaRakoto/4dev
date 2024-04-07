fc323d151f15fa0a156d0af205022764ea493dc8
raja <raja.rakoto7@gmail.com>
2024-04-07 11:24:41 +0300

    init build file

---
+++ build.js
@@ -0,0 +1,21 @@
+import dts from "bun-plugin-dts";
+
+// build process
+await Bun.build({
+	entrypoints: ["./src/index.ts"],
+	outdir: "./dist",
+	minify: true,
+	splitting: false,
+	sourcemap: "external",
+	target: "bun", // or 'node', 'browser'
+	plugins: [dts()],
+}).catch(console.error);
+
+// shebang process
+async function shebangProcess(filePath, shebang) {
+	const originalContent = await Bun.file(filePath).text();
+	const contentWithShebang = `${shebang}\n${originalContent}`;
+	await Bun.write(filePath, contentWithShebang);
+}
+
+// shebangProcess('./dist/index.js', '#!/usr/bin/env node');
