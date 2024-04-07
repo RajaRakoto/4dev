7ac04819d8aa06aba1486303c6b48b0516006e3f
raja <raja.rakoto7@gmail.com>
2024-04-07 11:20:34 +0300

    remove codebase

--- blinker/script.js
+++
@@ -1,5 +0,0 @@
-let blink_speed = 700;
-let t1 = setInterval(function () {
-	let ele = document.getElementById('blinker');
-	ele.style.visibility = ele.style.visibility == 'hidden' ? '' : 'hidden';
-}, blink_speed);
