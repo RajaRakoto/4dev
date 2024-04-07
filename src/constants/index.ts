44e41ac15c13bc8260cddb5801cac9dc0d8c7b1d
raja <raja.rakoto7@gmail.com>
2024-04-07 12:57:33 +0300

    src: init 4dev constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,37 @@
-// api
-export const BASE_URL = "https://api.example.com";
+// dev
+export const DEVMODE = true;
+
+// notes
+export const note1 = "⭐🟊🟊🟊🟊";
+export const note2 = "⭐⭐🟊🟊🟊";
+export const note3 = "⭐⭐⭐🟊🟊";
+export const note4 = "⭐⭐⭐⭐🟊";
+export const note5 = "⭐⭐⭐⭐⭐";
+
+// emoji
+export const emojiSearch = "🔍";
+export const emojiCategory = "📌";
+export const emojiLink = "🔗";
+export const emojiTools = "🛠️";
+export const emojiMessage = "💬";
+
+// table
+export const tableHeader = "| Liens | Descriptions | Notes |";
+export const tableSeparator1 = "| :-- |";
+export const tableSeparator3 = "| :-- | :-- | :-- |";
+
+// banner
+export const title = `# 4dev ${emojiTools}`;
+export const badge = `
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/for-you.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/open-source.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/uses-git.svg)](https://forthebadge.com) [![forthebadge](https://github.com/RajaRakoto/github-docs/blob/master/badge/build-by.svg?raw=true)](https://forthebadge.com)
+
+![Git](https://img.shields.io/badge/-Git-777?style=flat&logo=git&logoColor=F05032&labelColor=ffffff) ![Gitub](https://img.shields.io/badge/-Gitub-777?style=flat&logo=github&logoColor=777&labelColor=ffffff)
+`;
+export const message = `${emojiMessage} Vous pouvez également contribuer à ajouter/créer votre propre collection dans ce référentiel ...`
+
+// others
+export const backToTop = `
+<div align="left">
+    <b><a href="#table-des-matières">↥ Back To Top</a></b>
+</div>
+`
