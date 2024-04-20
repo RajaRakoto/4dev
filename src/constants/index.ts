a9dd016214546c3d241a06b978b9dc7194a1ea91
raja <raja.rakoto7@gmail.com>
2024-04-20 15:17:58 +0300

    src: hotfix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -25,9 +25,9 @@ export const title = `# 4dev ${emojiTools}`;
 export const badge = `
 [![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/for-you.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/open-source.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/uses-git.svg)](https://forthebadge.com) [![forthebadge](https://github.com/RajaRakoto/github-docs/blob/master/badge/build-by.svg?raw=true)](https://forthebadge.com)
 
-![Git](https://img.shields.io/badge/-Git-777?style=flat&logo=git&logoColor=F05032&labelColor=ffffff) ![Gitub](https://img.shields.io/badge/-Gitub-777?style=flat&logo=github&logoColor=777&labelColor=ffffff)
-`;
-export const message = `${emojiMessage} You can also contribute to add/create your own collection in this repository...`;
+![Git](https://img.shields.io/badge/-Git-777?style=flat&logo=git&logoColor=F05032&labelColor=ffffff) ![Gitub](https://img.shields.io/badge/-Gitub-777?style=flat&logo=github&logoColor=777&labelColor=ffffff)`;
+
+export const message = `${emojiMessage} You can also contribute to add/create your own collection in this repository ...`;
 
 // others
 export const backToTop = `
