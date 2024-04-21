da07dde5b755ee5e7dd1cefe32e5cc7cacc07aa0
raja <raja.rakoto7@gmail.com>
2024-04-21 20:22:10 +0300

    src: add description -> constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -30,6 +30,9 @@ export const badge = `
 [![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/for-you.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/open-source.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/uses-git.svg)](https://forthebadge.com) [![forthebadge](https://github.com/RajaRakoto/github-docs/blob/master/badge/build-by.svg?raw=true)](https://forthebadge.com)
 
 ![Git](https://img.shields.io/badge/-Git-777?style=flat&logo=git&logoColor=F05032&labelColor=ffffff) ![Gitub](https://img.shields.io/badge/-Gitub-777?style=flat&logo=github&logoColor=777&labelColor=ffffff)`;
+export const description = `
+**4dev** is a versatile opensource toolkit tailored to meet diverse development demands, with a primary emphasis on web development. Boasting a rich collection of tools and resources, it equips developers with the essentials needed to streamline their workflow and enhance productivity.
+`;
 export const contrib = `
 ${emojiMessage} You can also add/create your own collection in this repository by following the steps below:
 
