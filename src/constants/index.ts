2f9fed56ac9ec0863b032e64fa8f794a3996c044
raja <raja.rakoto7@gmail.com>
2024-04-21 14:04:51 +0300

    src: update emoji | pre valid contrib -> constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,7 +12,8 @@ export const note4 = "⭐⭐⭐⭐🟊";
 export const note5 = "⭐⭐⭐⭐⭐";
 
 // emoji
-export const emojiCategory = "📌";
+export const emojiTitle = "📌";
+export const emojiCategory = "⚫";
 export const emojiLink = "🔗";
 export const emojiTools = "🛠️";
 export const emojiMessage = "💬";
@@ -29,8 +30,44 @@ export const badge = `
 [![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/for-you.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/open-source.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/uses-git.svg)](https://forthebadge.com) [![forthebadge](https://github.com/RajaRakoto/github-docs/blob/master/badge/build-by.svg?raw=true)](https://forthebadge.com)
 
 ![Git](https://img.shields.io/badge/-Git-777?style=flat&logo=git&logoColor=F05032&labelColor=ffffff) ![Gitub](https://img.shields.io/badge/-Gitub-777?style=flat&logo=github&logoColor=777&labelColor=ffffff)`;
+export const contrib = `
+${emojiMessage} You can also add/create your own collection in this repository by following the steps below:
 
-export const message = `${emojiMessage} You can also contribute to add/create your own collection in this repository ...`;
+> All collections are stored in the \`src/data\` directory. Each collection is a JSON file that contains the following fields:
+
+\`\`\`json
+{
+    "name": "collection name",
+    "keywords": ["keyword1", "keyword2", "keyword3"],
+    "url": "https://collection.io",
+    "ref": "",
+    "description": "A description of the collection ...",
+    "note": 5
+}
+\`\`\`
+
+**STEP 1: Add/Create**
+
+- A JSON file located in the \`src/data\` directory represents a category. If your collection corresponds to an existing category, you can add it directly to the list of existing collections. Otherwise, you have the option to create a new category by creating a new JSON file and building your own collection.
+
+- The first keyword should always be the most relevant to the collection, and it should also be similar to the category name to facilitate collection organization and search.
+
+**STEP 2: Check/Build**
+
+- To check the validity of your collection, simply run the following command. If there are any errors, they will be displayed; otherwise, the collection will be built into the README.md file:
+
+\`\`\`bash
+bun run build
+\`\`\`
+
+**STEP 3: Format**
+
+- Before commiting your collection, make sure that are formatted correctly with the following command:
+
+\`\`\`bash
+bun run prettier
+\`\`\`
+`;
 
 // others
 export const backToTop = `
